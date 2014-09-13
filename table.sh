8b7bac2039d5a1980bb6cd206e9b610b5e6e1e0a
Martin Hammer <martin.hammer@outlook.com>
2014-09-13 11:30:05 +0400

    Fixed column offset issue in table

--- table.sh
+++ table.sh
@@ -11,7 +11,7 @@ WDATETIME=`date +"%Y%m%d"`
 wget -q -O $WDOWNLOAD_PATH/table.shtml http://news.bbc.co.uk/mobile/bbc_sport/football/competition/100/table/index.shtml
 
 # create plain text version
-grep '<h2><abbr title="Barclays Premier League"><span>Premier League</span></abbr></h2>' $WDOWNLOAD_PATH/table.shtml | sed -e 's/\(^.*<span>Premier League<\/span><\/abbr><\/h2> \)\(.*\)\(<span>Championship<\/span>.*$\)/\2/' | sed -e 's/\(^.*<\/thead> <tbody>\)\(.*\)\(<\/tbody> <\/table>\)\(.*$\)/\2/' | sed -e 's/<\/tr>/\n/g' | sed -e 's/<[^>]*>/\t/g' | tr -s '\t' '\t' | cut -sf 3,5,7,9,11,13,15,17 > $WDOWNLOAD_PATH/table.$WDATETIME
+grep '<h2><abbr title="Barclays Premier League"><span>Premier League</span></abbr></h2>' $WDOWNLOAD_PATH/table.shtml | sed -e 's/\(^.*<span>Premier League<\/span><\/abbr><\/h2> \)\(.*\)\(<span>Championship<\/span>.*$\)/\2/' | sed -e 's/\(^.*<\/thead> <tbody>\)\(.*\)\(<\/tbody> <\/table>\)\(.*$\)/\2/' | sed -e 's/<\/tr>/\n/g' | sed -e 's/<[^>]*>/\t/g' | tr -s '\t' '\t' | cut -sf 3,6,8,10,12,14,16,18 > $WDOWNLOAD_PATH/table.$WDATETIME
 
 # create html version
 echo -e "<html>\n<body style='font-family:monospace'>\n<table border=1>\n<tr><th>Pos</th><th>Team</th><th>P</th><th>W</th><th>D</th><th>L</th><th>GD</th><th>Pts</th></tr>" > $WDOWNLOAD_PATH/table.html
